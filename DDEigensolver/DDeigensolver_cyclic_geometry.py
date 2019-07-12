e7b999c1dffe0c4996471df48dc8758ff06bc44d
Guilherme Jenovencio <guilherme.jenovencio@tum.de>
2019-07-12 17:54:14 +0200

    update

--- DDEigensolver/DDeigensolver_cyclic_geometry.py
+++ DDEigensolver/DDeigensolver_cyclic_geometry.py
@@ -168,7 +168,7 @@ save_object(Lexp,'Lexp.pkl')
 
 Kp = L.dot(K.dot(Lexp))
 Mp = L.dot(M.dot(Lexp))
-lu = sparse.splu(Kp)
+lu = sparse.linalg.splu(Kp.tocsc())
 Dp = sparse.LinearOperator(shape=Kp.shape, matvec = lambda  x : lu.solve(Mp.dot(x)))
 nmodes = 5
 
